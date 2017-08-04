50e10227204b1bb62eb0cae9a8235fd096454c77
Evgenii <magicwolf9@mail.ru>
2017-08-04 16:18:44 +0300

    Outputs differences for 2 test rows

--- app/controllers/baseController.ts
+++ app/controllers/baseController.ts
@@ -1,17 +1,141 @@
 import {Controller} from "innots";
 import {Context} from 'koa';
-import {KassaTestModel, ExampleTableRow} from "../models/kassa_test";
+import {KassaTestModel} from "../models/kassa_test";
 
 export class BaseController extends  Controller{
 
+    table1Data: Array<any>;
+    table2Data: Array<any>;
+
+    table1Name: string;
+    table2Name: string;
+
     table = async (ctx: Context, next: () => any): Promise<void> => {
 
-        let tableData: Array<ExampleTableRow> = await KassaTestModel.getAll('kassa_test');
+        this.table1Name = 'kassa_test';
+        this.table2Name = 'kassa_test';
 
-        console.log();
+        let i;
+        let hasCode: boolean;
+        let k;
+        this.table1Data = await KassaTestModel.getAll(this.table1Name);
+        this.table2Data = await KassaTestModel.getAll1(this.table2Name);
 
-        ctx.body = tableData;
+        let differences: Array<IDifference> = [];
+
+        // {[codes], [codes]} {[values], [values]}
+        // codes map ---> loop "find" else make a diff
+        //                                             if true check values
+
+        let table1Columns: Array<string> = this.table1Data.map(a => a.extra_code);
+        let table2Columns: Array<string> = this.table2Data.map(a => a.extra_code);
+
+        // make hashmap column-value
+        let table1HashMap = this.table1Data.reduce(function(map, obj) {
+            map[obj.extra_code] = obj.extra_name;
+            return map;
+        }, {});
+        let table2HashMap = this.table2Data.reduce(function(map, obj) {
+            map[obj.extra_code] = obj.extra_name;
+            return map;
+        }, {});
+
+        let uniqueColumnsInTable1 = table1Columns.filter(val => table2Columns.indexOf(val) == -1);
+        let uniqueColumnsInTable2 = table2Columns.filter(val => table1Columns.indexOf(val) == -1);
+
+        // make differences from unique columns and delete them from hashmap
+        for(let columnName of uniqueColumnsInTable1){
+            differences[differences.length] = {
+                table1: this.table1Name,
+                table2: this.table2Name,
+
+                columnInTable1: columnName,
+                columnInTable2: '',
+
+                rowInTable1: -1,
+                rowInTable2: -1,
+
+                valueInTable1: table1HashMap[columnName],
+                valueInTable2: '',
+            };
+            delete table1HashMap[columnName];
+        }
+
+        for(let columnName of uniqueColumnsInTable2){
+            differences[differences.length] = {
+                table1: this.table1Name,
+                table2: this.table2Name,
+
+                columnInTable1: '',
+                columnInTable2: columnName,
+
+                rowInTable1: -1,
+                rowInTable2: -1,
+
+                valueInTable1: '',
+                valueInTable2: table2HashMap[columnName],
+            };
+            delete table2HashMap[columnName];
+        }
+
+        // check columns for equal values, make differences is not
+        Object.keys(table1HashMap).forEach((key: string) => {
+            const value = table1HashMap[key];
+            if(table2HashMap[key] != value){
+                differences[differences.length] = {
+                    table1: this.table1Name,
+                    table2: this.table2Name,
+
+                    columnInTable1: key,
+                    columnInTable2: key,
+
+                    rowInTable1: 1,
+                    rowInTable2: 1,
+
+                    valueInTable1: value,
+                    valueInTable2: table2HashMap[key],
+                };
+            }
+        });
+
+        ctx.body = differences;
         next();
+    };
+
+    makeDifference(table1row?: number, table2row?: number): IDifference{
+        let newDiff: IDifference;
+
+        newDiff.table1 = this.table1Name;
+        newDiff.table2 = this.table2Name;
+
+
+        newDiff.rowInTable1 = table1row;
+        newDiff.rowInTable2 = table2row;
+
+        if(table1row && table2row) {
+            newDiff.columnInTable1 = this.table1Data[table1row].extra_code;
+            newDiff.columnInTable2 = this.table2Data[table2row].extra_code;
+
+            newDiff.valueInTable1 = this.table1Data[table1row].extra_name;
+            newDiff.valueInTable2 = this.table2Data[table2row].extra_name;
+        }
+        return newDiff;
     }
 
+}
+
+
+
+interface IDifference{
+    table1: string;
+    table2: string;
+
+    columnInTable1: string;
+    columnInTable2: string;
+
+    rowInTable1: number;
+    rowInTable2: number;
+
+    valueInTable1: any;
+    valueInTable2: any;
 }
--- app/models/kassa_test.ts
+++ app/models/kassa_test.ts
@@ -3,14 +3,346 @@ import {pgService} from "../../globals";
 
 const defaultSchema = config.get('defaultSchema');
 
-export interface ExampleTableRow{
+/*export interface ExampleTableRow{
     colOne: string;
     colTwo: string;
-}
+}*/
 
-export  class KassaTestModel{
-    // [{colOne: '', colTwo: ''}];
-    static async getAll(tableName: string): Promise<ExampleTableRow[]>{
-        return await pgService.getRows(`SELECT columnOne, columnTwo FROM ${defaultSchema}.example_table`);
+
+export class KassaTestModel {
+    // [{colOne: '', colTwo: ''}, ];
+    static async getAll(tableName: string): Promise<Array<any>> {
+
+        return [
+            {
+                extra_code: "ISSUE_DATE",
+                extra_name: "ДАТА ВЫДАЧИ ПАСПОРТА"
+            },
+            {
+                extra_code: "ISSUED_BY",
+                extra_name: "КЕМ ВЫДАН"
+            },
+            {
+                extra_code: "KPP",
+                extra_name: "КПП"
+            },
+            {
+                extra_code: "CHIEF_ISSUE_DATE",
+                extra_name: "ДАТА ВЫДАЧИ ПАСПОРТА РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "CHIEF_ISSUED_BY",
+                extra_name: "КЕМ ВЫДАН ПАСПОРТ РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "OGRN",
+                extra_name: "ОГРН"
+            },
+            {
+                extra_code: "OKPO",
+                extra_name: "ОКПО"
+            },
+            {
+                extra_code: "MAIL_ADDR",
+                extra_name: "ПОЧТОВЫЙ АДРЕС"
+            },
+            {
+                extra_code: "CHIEF_BIRTHDATE",
+                extra_name: "ДАТА РОЖДЕНИЯ РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "BIC",
+                extra_name: "БИК"
+            },
+            {
+                extra_code: "OPER_ACCOUNT",
+                extra_name: "РАСЧЕТНЫЙ СЧЕТ"
+            },
+            {
+                extra_code: "CORR_ACCOUNT",
+                extra_name: "КОРРЕСПОНДЕНТСКИЙ СЧЕТ"
+            },
+            {
+                extra_code: "BANK_NAME",
+                extra_name: "НАЗВАНИЕ БАНКА"
+            },
+            {
+                extra_code: "FORM_OF_INCORPORATION",
+                extra_name: "ОРГАНИЗАЦИОННО-ПРАВОВА ФОРМА"
+            },
+            {
+                extra_code: "ACTUAL_ADDR",
+                extra_name: "ФАКТИЧЕСКИЙ АДРЕС"
+            },
+            {
+                extra_code: "LEGAL_ADDR",
+                extra_name: "ЮРИДИЧЕСКИЙ АДРЕС"
+            },
+            {
+                extra_code: "COINCIDE_WITH_LEGAL",
+                extra_name: "ФАКТИЧЕСКИЙ АДРЕС СОВПАДАЕТ С ЮРИДИЧЕСКИМ"
+            },
+            {
+                extra_code: "CHIEF_SURNAME",
+                extra_name: "ФАМИЛИЯ РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "REPRESENTATIVE_PHONE",
+                extra_name: "ТЕЛЕФОН ПРЕДСТАВИТЕЛЯ (КОНТАКТНОЕ ЛИЦО)"
+            },
+            {
+                extra_code: "REPRESENTATIVE_EMAIL",
+                extra_name: "ПОЧТОВЫЙ АДРЕС ПРЕДСТАВИТЕЛЯ (КОНТАКТНОЕ ЛИЦО)"
+            },
+            {
+                extra_code: "PARTNER_NAME",
+                extra_name: "ЮРИДИЧЕСКОЕ НАИМЕНОВАНИЕ КОМПАНИИ"
+            },
+            {
+                extra_code: "SITE_PURPOSE",
+                extra_name: "ПЕРЕЧЕНЬ ТОВАРОВ"
+            },
+            {
+                extra_code: "REPRESENTATIVE_NAME",
+                extra_name: "ИМЯ ПРЕДСТАВИТЕЛЯ (КОНТАКТНОЕ ЛИЦО)"
+            },
+            {
+                extra_code: "REPRESENTATIVE_SURNAME",
+                extra_name: "ФАМИЛИЯ ПРЕДСТАВИТЕЛЯ (КОНТАКТНОЕ ЛИЦО)"
+            },
+            {
+                extra_code: "REPRESENTATIVE_PATRONYMIC",
+                extra_name: "ОТЧЕСТВО ПРЕДСТАВИТЕЛЯ (КОНТАКТНОЕ ЛИЦО)"
+            },
+            {
+                extra_code: "CHIEF_PASSPORT_NUMB",
+                extra_name: "НОМЕР ПАСПОРТА РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "CHIEF_PATRONYMIC",
+                extra_name: "ОТЧЕСТВО РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "REGISTRATION_AUTHORITY",
+                extra_name: "РЕГИСТРИРУЮЩИЙ ОРГАН"
+            },
+            {
+                extra_code: "REGISTRATION_DATE",
+                extra_name: "ДАТА РЕГИСТРАЦИИ"
+            },
+            {
+                extra_code: "REGISTRATION_CITY",
+                extra_name: "ГОРОД РЕГИСТРАЦИИ"
+            },
+            {
+                extra_code: "CHIEF_NAME",
+                extra_name: "ИМЯ РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "CHIEF_BIRTHPLACE",
+                extra_name: "МЕСТО РОЖДЕНИЕ РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "CHIEF_PASSPORT_SER",
+                extra_name: "СЕРИЯ ПАСПОРТА РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "COINCIDE_WITH_CHIEF",
+                extra_name: "КОНТАКТНЫМ ЛИЦОМ ЯВЛЯЕТСЯ РУКОВОДИТЕЛЬ"
+            },
+            {
+                extra_code: "DEPARTMENT_CODE",
+                extra_name: "КОД ПОДРАЗДЕЛЕНИЯ"
+            },
+            {
+                extra_code: "MARKETING_CATEGORY_ID",
+                extra_name: "НОМЕР МАРКЕТИНГОВОЙ КАТЕГОРИИ"
+            },
+            {
+                extra_code: "MARKETING_CATEGORY_NAME",
+                extra_name: "МАРКЕТИНГОВАЯ КАТЕГОРИЯ"
+            },
+            {
+                extra_code: "MARKETING_CATEGORY",
+                extra_name: "МАРКЕТИНГОВАЯ КАТЕГОРИЯ"
+            },
+            {
+                extra_code: "CONTRACT_NUMBER",
+                extra_name: "НОМЕР ДОГОВОРА"
+            }
+        ];
+
+        //return await pgService.getRows(`SELECT * FROM ${defaultSchema}.example_table`);
+
+        // return await pgService.getRows(`SELECT columnOne, columnTwo FROM ${defaultSchema}.example_table`);
     }
+
+    static async getAll1(tableName: string): Promise<Array<any>> {
+
+        return await [
+            {
+                extra_code: "MERCHANT_SITE_EXTERNAL_ID",
+                extra_name: "ИДЕНТИФИКАТОР САЙТА"
+            },
+            {
+                extra_code: "ISSUE_DATE",
+                extra_name: "ДАТА ВЫДАЧИ ПАСПОРТА"
+            },
+            {
+                extra_code: "ISSUED_BY",
+                extra_name: "КЕМ ВЫДАН"
+            },
+            {
+                extra_code: "KPP",
+                extra_name: "КПП"
+            },
+            {
+                extra_code: "CHIEF_ISSUE_DATE",
+                extra_name: "ДАТА ВЫДАЧИ ПАСПОРТА РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "CHIEF_ISSUED_BY",
+                extra_name: "КЕМ ВЫДАН ПАСПОРТ РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "OGRN",
+                extra_name: "ОГРН"
+            },
+            {
+                extra_code: "OKPO",
+                extra_name: "ОКПО"
+            },
+            {
+                extra_code: "MAIL_ADDR",
+                extra_name: "ПОЧТОВЫЙ АДРЕС"
+            },
+            {
+                extra_code: "CHIEF_BIRTHDATE",
+                extra_name: "ДАТА РОЖДЕНИЯ РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "BIC",
+                extra_name: "БИК"
+            },
+            {
+                extra_code: "OPER_ACCOUNT",
+                extra_name: "РАСЧЕТНЫЙ СЧЕТ"
+            },
+            {
+                extra_code: "CORR_ACCOUNT",
+                extra_name: "КОРРЕСПОНДЕНТСКИЙ СЧЕТ"
+            },
+            {
+                extra_code: "BANK_NAME",
+                extra_name: "НАЗВАНИЕ БАНКА"
+            },
+            {
+                extra_code: "FORM_OF_INCORPORATION",
+                extra_name: "ОРГАНИЗАЦИОННО-ПРАВОВА ФОРМА"
+            },
+            {
+                extra_code: "ACTUAL_ADDR",
+                extra_name: "ФАКТИЧЕСКИЙ АДРЕС"
+            },
+            {
+                extra_code: "LEGAL_ADDR",
+                extra_name: "ЮРИДИЧЕСКИЙ АДРЕС"
+            },
+            {
+                extra_code: "COINCIDE_WITH_LEGAL",
+                extra_name: "ФАКТИЧЕСКИЙ АДРЕС СОВПАДАЕТ С ЮРИДИЧЕСКИМ"
+            },
+            {
+                extra_code: "CHIEF_SURNAME",
+                extra_name: "ФАМИЛИЯ РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "REPRESENTATIVE_PHONE",
+                extra_name: "ТЕЛЕФОН ПРЕДСТАВИТЕЛЯ (КОНТАКТНОЕ ЛИЦО)"
+            },
+            {
+                extra_code: "REPRESENTATIVE_EMAIL",
+                extra_name: "ПОЧТОВЫЙ АДРЕС ПРЕДСТАВИТЕЛЯ (КОНТАКТНОЕ ЛИЦО)"
+            },
+            {
+                extra_code: "PARTNER_NAME",
+                extra_name: "ЮРИДИЧЕСКОЕ НАИМЕНОВАНИЕ КОМПАНИИ"
+            },
+            {
+                extra_code: "SITE_PURPOSE",
+                extra_name: "ПЕРЕЧЕНЬ ТОВАРОВ"
+            },
+            {
+                extra_code: "REPRESENTATIVE_NAME",
+                extra_name: "ИМЯ ПРЕДСТАВИТЕЛЯ (КОНТАКТНОЕ ЛИЦО)"
+            },
+            {
+                extra_code: "REPRESENTATIVE_SURNAME",
+                extra_name: "ФАМИЛИЯ ПРЕДСТАВИТЕЛЯ (КОНТАКТНОЕ ЛИЦО)"
+            },
+            {
+                extra_code: "REPRESENTATIVE_PATRONYMIC",
+                extra_name: "ОТЧЕСТВО ПРЕДСТАВИТЕЛЯ (КОНТАКТНОЕ ЛИЦО)"
+            },
+            {
+                extra_code: "CHIEF_PASSPORT_NUMB",
+                extra_name: "НОМЕР ПАСПОРТА РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "CHIEF_PATRONYMIC",
+                extra_name: "ОТЧЕСТВО РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "REGISTRATION_AUTHORITY",
+                extra_name: "РЕГИСТРИРУЮЩИЙ ОРГАН"
+            },
+            {
+                extra_code: "REGISTRATION_DATE",
+                extra_name: "ДАТА РЕГИСТРАЦИИ"
+            },
+            {
+                extra_code: "REGISTRATION_CITY",
+                extra_name: "ГОРОД РЕГИСТРАЦИИ"
+            },
+            {
+                extra_code: "CHIEF_NAME",
+                extra_name: "ИМЯ РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "CHIEF_BIRTHPLACE",
+                extra_name: "МЕСТО РОЖДЕНИЕ РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "CHIEF_PASSPORT_SER",
+                extra_name: "СЕРИЯ ПАСПОРТА РУКОВОДИТЕЛЯ"
+            },
+            {
+                extra_code: "COINCIDE_WITH_CHIEF",
+                extra_name: "КОНТАКТНЫМ ЛИЦОМ ЯВЛЯЕТСЯ РУКОВОДИТЕЛЬ"
+            },
+            {
+                extra_code: "DEPARTMENT_CODE",
+                extra_name: "КОД ПОДРАЗДЕЛЕНИЯ"
+            },
+            {
+                extra_code: "MARKETING_CATEGORY_ID",
+                extra_name: "НОМЕР МАРКЕТИНГОВОЙ КАТЕГОРИИ"
+            },
+            {
+                extra_code: "MARKETING_CATEGORY_NAME",
+                extra_name: "МАРКЕТИНГОВАЯ КАТЕГОРИЯ"
+            },
+            {
+                extra_code: "MARKETING_CATEGORY",
+                extra_name: "МАРКЕТИНГОВАЯ КАТЕГОРИЯ"
+            }
+        ];
+
+
+        //return await pgService.getRows(`SELECT * FROM ${defaultSchema}.example_table`);
+
+        // return await pgService.getRows(`SELECT columnOne, columnTwo FROM ${defaultSchema}.example_table`);
+    }
+
+
 }
