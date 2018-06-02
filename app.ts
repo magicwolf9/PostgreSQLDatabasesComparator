dd8971cd9a6166706902a40fbaba9f0f627c8343
Evgenii <magicwolf9@mail.ru>
2018-06-02 21:30:57 +0300

    Updated innots to version 1.1.11

--- app.ts
+++ app.ts
@@ -5,4 +5,13 @@ import { IAppConfig, InnotsApp, PgPool, PgService } from "innots";
 import { router } from "./app/routes";
 
 const app = new InnotsApp(config.get<IAppConfig>('appConfig'), router);
+
+app.bootstrap()
+    .then(() => {
+        console.log('server is listening on port', config.get<IAppConfig>('appConfig').port);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+    
 export{app}
